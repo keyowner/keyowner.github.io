621c36dfbdbf4008e8117d092013e3103956ea18
keyowner <gusynayapesnya@gmail.com>
2024-11-10 03:54:44 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 tg.disableVerticalSwipes()
 //tg.viewportHeight
-let tgid=6761355325//pngwindow.Telegram.WebApp.initDataUnsafe.user.id
-let f_name='[key_owner]'//window.Telegram.WebApp.initDataUnsafe.user.first_name
+let tgid=window.Telegram.WebApp.initDataUnsafe.user.id
+let f_name=window.Telegram.WebApp.initDataUnsafe.user.first_name
 
 const app = new PIXI.Application();
 await app.init({ resizeTo: window, autoDensity: true});
@@ -2492,7 +2492,7 @@ app.ticker.add(() => {
                         }    
                     }
                     else {
-                        if (score>my_hard_highscore){
+                        if (my_score>my_hard_highscore){
                             my_hard_highscore=score
                         }  
                     }
